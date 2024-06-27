6a330ae80b1328e38efb098757b724ae98bc3ace
Soumik-Sarkar <61474876+Soumik-Sarkar@users.noreply.github.com>
2024-06-27 14:05:27 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,53 +3,25 @@ import { MindARThree } from "mindar-image-three";
 
 const mindarThree = new MindARThree({
   container: document.querySelector("#container"),
-  imageTargetSrc: "targets.mind",
+  imageTargetSrc: "targets (1).mind",
 });
 const { renderer, scene, camera } = mindarThree;
 const anchor = mindarThree.addAnchor(0);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 //const geometry = new THREE.PlaneGeometry(1, 0.55);
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ffff,
-  transparent: true,
-  opacity: 0.5,
-});
+const material = new THREE.MeshBasicMaterial();
 const plane = new THREE.Mesh(geometry, material);
 anchor.group.add(plane);
 
-const isMobileDevice = () => /Mobi|Android/i.test(navigator.userAgent);
-const isIOS = () => /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
-
-const getVideoConstraints = () => {
-  if (isIOS()) {
-    return {
-      video: {
-        facingMode: { exact: "environment" },
-      },
-    };
-  } else if (isMobileDevice()) {
-    return {
-      video: {
-        facingMode: { exact: "environment" },
-      },
-    };
-  } else {
-    return {
-      video: {
-        facingMode: "user",
-      },
-    };
-  }
-};
-
-
 const start = async () => {
-  // await mindarThree.start();
-  // renderer.setAnimationLoop(() => {
-  //   renderer.render(scene, camera);
-  // });
   try {
-    const stream = await navigator.mediaDevices.getUserMedia(getVideoConstraints());
+    const constraints = {
+      video: {
+        facingMode: isMobileDevice() ? { exact: "environment" } : "user",
+      },
+    };
+
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
     const video = document.createElement("video");
     video.srcObject = stream;
     await video.play();
@@ -65,11 +37,12 @@ const start = async () => {
     console.error("Error accessing the camera: ", err);
   }
 };
-// const startButton = document.querySelector("#startButton");
-// startButton.addEventListener("click", () => {
-  start();
-// });
-// stopButton.addEventListener("click", () => {
-//   mindarThree.stop();
-//   mindarThree.renderer.setAnimationLoop(null);
-// });
+
+const isMobileDevice = () => {
+  return (
+    typeof window.orientation !== "undefined" ||
+    navigator.userAgent.indexOf("IEMobile") !== -1
+  );
+};
+
+start();
